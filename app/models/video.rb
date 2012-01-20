53449f1875c43dc198c425e55a9ecf0ca2c6e29a
Alexey Krasnoperov <matsarello@mail.ru>
2012-01-20 19:23:24 +0400

    GET Video REST ok

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,16 +2,24 @@ class Video < ActiveRecord::Base
 	
 	has_attached_file :uploaded_video, :styles => { 
       :flv => { :geometry => "640x480", :format => 'flv' },
-      :mp4 => { :geometry => '640x480', :format => 'mp4',
-        :convert_options => { :output => { :vcodec => 'libx264',
-          :vpre => 'ipod640', :b => '250k', :bt => '50k',
-          :acodec => 'libfaac', :ab => '56k', :ac => 2 } } },
-      :ogg => { :geometry => '640x480', :format => 'ogg',
-        :convert_options => { :output => { :vcodec => 'libtheora',
-          :b => '250k', :bt => '50k', :acodec => 'libvorbis',
-          :ab => '56k', :ac => 2 } } },
       :preview => { :geometry => "100x100#", :format => 'jpg', :time => 1 }
     }, :processors => [:ffmpeg]
 		validates_attachment_content_type :uploaded_video, :content_type => ['video/x-msvideo','video/avi','video/quicktime','video/3gpp','video/x-ms-wmv','video/mp4','video/mpeg']
     process_in_background :uploaded_video
+	
+	def as_json(options={})
+		# :only (explicit list of attributes)
+		# :except (attribute exclusion list)
+		# :methods (list of methods the execute and include their content)
+		# :include (relations)
+	  super(:methods => [:preview_url, :flv_url])
+	end
+	
+	def preview_url
+		uploaded_video.url(:preview)
+	end
+	
+	def flv_url
+		uploaded_video.url(:flv)
+	end
 end
